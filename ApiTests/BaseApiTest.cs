aeef06a56e8e6cac3c5f2c878782dc021e65fc80
Никита <isthisnikita@gmail.com>
2023-07-20 00:22:21 +0300

    Base API solution

---
+++ ApiTests/BaseApiTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core;
+
+namespace UnitTests.ApiTests
+{
+    internal class BaseApiTest
+    {
+    
+        protected BaseApiClient apiClient;
+
+        [OneTimeSetUp]
+        public void InitApiClient()
+        {
+          // apiClient = new BaseApiClient("https://isthisnikita.testrail.io/index.php?/api/v2");
+          //  apiClient.AddToken("vd0SC5SATgakfaOApOYa-DJPIScT3dEp1AzUgVms5");
+        }
+
+    }
+}
---
+++ ApiTests/CaseTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObject.ApiObjects.Services;
+
+namespace UnitTests.ApiTests
+{
+    internal class CaseTests : BaseApiTest
+    {
+
+        protected TestCaseService caseService;
+
+
+        [OneTimeSetUp]
+        public void InitialService()
+        {
+            caseService = new TestCaseService();
+        }
+
+        [Test]
+        public void GetAlltestCases()
+        {
+            var caseCode = "1";
+
+            var response = caseService.GetAlltestCases(caseCode);
+            Console.WriteLine(response.Content);
+            Assert.IsTrue(response.StatusCode.Equals(HttpStatusCode.OK));
+        }
+        [Test]
+        public void DeleteTestCase()
+        {
+            var caseCode = "TESTM";
+            var id = 3;
+            var response = caseService.DeleteTestCase(caseCode, id);
+            Console.WriteLine(response.Content);
+            Assert.IsTrue(response.StatusCode.Equals(HttpStatusCode.OK));
+        }
+        [Test]
+        public void UpdateTestCase()
+        {
+            var caseCode = "TESTM";
+            var id = 2;
+            var newTitle = "new_test_case2";
+
+            var response = caseService.UpdateTestCase(caseCode, id, newTitle);
+            Console.WriteLine(response.Content);
+            Assert.IsTrue(response.StatusCode.Equals(HttpStatusCode.OK));
+        }
+        [Test]
+        public void CreateTestCasesinBulk()
+        {
+            var caseCode = "TESTM";
+            var title = "test_case_bulk";
+
+            var response = caseService.CreateTestCasesinBulk(caseCode, title);
+            Console.WriteLine(response.Content);
+            Assert.IsTrue(response.StatusCode.Equals(HttpStatusCode.OK));
+        }
+        [Test]
+        public void CreateANewTestCase()
+        {
+            var caseCode = "TESTM";
+            var title = "test_case";
+            var response = caseService.CreateANewTestCase(caseCode, title);
+            Console.WriteLine(response.Content);
+            Assert.IsTrue(response.StatusCode.Equals(HttpStatusCode.OK));
+        }
+        [Test]
+        public void GetASpecificTestCase()
+        {
+            var caseCode = "TESTM";
+            var id = 2;
+
+            var response = caseService.GetASpecificTestCase(caseCode, id);
+            Console.WriteLine(response.Content);
+            Assert.IsTrue(response.StatusCode.Equals(HttpStatusCode.OK));
+        }
+    }
+}
